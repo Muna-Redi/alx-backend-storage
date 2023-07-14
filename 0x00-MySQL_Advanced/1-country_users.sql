c5b893c1260c9ef594d6585be35283a67cf9861a
Muna-Redi <francischym@gmail.com>
2023-07-14 04:59:19 +0100

    table users with new requirements

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- This MySQL script creates a table (called user) with
+-- fields id, email, name, country
+CREATE TABLE IF NOT EXISTS users(
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255),
+	country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+);
